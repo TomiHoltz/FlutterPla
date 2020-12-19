f5c2062c520729e1e633b36557d7438eabc6a91a
Tomas Holtz <holtztomy@gmail.com>
2020-12-19 00:47:13 -0300

    Primer ¨Hola Mundo¨

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,16 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold (
+        appBar: AppBar(
+          title: Text('Hola Mundo')
+      ),
+        body: Center(
+          child: Text('Hola Mundo')
+        ),
+      )//MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
